ac3550937c14acb65420cd5e120049a1f353df3f
Borkesz Máté <borkeszmate@gmail.com>
2018-08-10 17:02:02 +0200

    Https API call

--- js/wunderground.js
+++ js/wunderground.js
@@ -1,7 +1,7 @@
 class Wunderground {
 
  async getWeather(city) {
-  let response = await fetch(`http://api.wunderground.com/api/7f21565d35515914/conditions/q/HU/${city}.json`);
+  let response = await fetch(`https://api.wunderground.com/api/7f21565d35515914/conditions/q/HU/${city}.json`);
 
   let resData = await response.json();
   return resData;
